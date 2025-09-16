1380a63b35c9c3736b8db54a03a9bb5d0c53bdae
Mohamed-Ashraf0z <m0hamed.0l0.357@gmail.com>
2025-09-17 00:56:59 +0300

    homeprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pharmacy_system/pages/mainpage/HomeScreen.dart';
+import 'package:pharmacy_system/pages/mainpage/controller/homeprovider.dart';
 import 'package:pharmacy_system/pages/subScreen-testonly/SubScreen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [], child: const MyApp()));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => HomeNotifier())
+  ], child:  MyApp()));
 }
 
 class MyApp extends StatelessWidget {
